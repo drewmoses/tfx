30df661849831b9a5b2149e5a8bacc7f4d799df8
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-09-09 15:35:06 -0700

    Internal change
    PiperOrigin-RevId: 330812077

--- tfx/components/example_gen/driver.py
+++ tfx/components/example_gen/driver.py
@@ -18,25 +18,36 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import copy
 import os
-from typing import Any, Dict, List, Text
-
+from typing import Any, Dict, List, Optional, Text
 from absl import logging
 from tfx import types
 from tfx.components.base import base_driver
 from tfx.components.example_gen import utils
 from tfx.orchestration import data_types
+from tfx.orchestration import metadata
+from tfx.orchestration.portable import base_driver as ir_base_driver
 from tfx.proto import example_gen_pb2
-
+from tfx.proto.orchestration import driver_output_pb2
+from tfx.proto.orchestration import pipeline_pb2
 from google.protobuf import json_format
 
 
-class Driver(base_driver.BaseDriver):
+class Driver(base_driver.BaseDriver, ir_base_driver.BaseDriver):
   """Custom driver for ExampleGen.
 
   This driver supports file based ExampleGen, e.g., for CsvExampleGen and
   ImportExampleGen.
   """
+
+  def __init__(self,
+               metadata_handler: metadata.Metadata,
+               pipeline_info: Optional[pipeline_pb2.PipelineInfo] = None,
+               pipeline_node: Optional[pipeline_pb2.PipelineNode] = None):
+    base_driver.BaseDriver.__init__(self, metadata_handler)
+    ir_base_driver.BaseDriver.__init__(
+        self, metadata_handler, pipeline_info, pipeline_node)
 
   def resolve_exec_properties(
       self,
@@ -98,3 +109,51 @@ class Driver(base_driver.BaseDriver):
     base_driver._prepare_output_paths(example_artifact)  # pylint: disable=protected-access
 
     return {utils.EXAMPLES_KEY: [example_artifact]}
+
+  def run(self, input_dict: Dict[Text, List[types.Artifact]],
+          output_dict: Dict[Text, List[types.Artifact]],
+          exec_properties: Dict[Text, Any]) -> driver_output_pb2.DriverOutput:
+    if not self._pipeline_info or not self._pipeline_node:
+      raise ValueError(
+          'pipeline_info and pipeline_node must be supplied in the IR mode.')
+
+    if utils.INPUT_CONFIG_KEY not in self._pipeline_node.parameters.parameters:
+      raise ValueError('input_config must be provided to example_gen')
+    input_config = example_gen_pb2.Input()
+    json_format.Parse(
+        self._pipeline_node.parameters.parameters[
+            utils.INPUT_CONFIG_KEY].field_value.string_value, input_config)
+    if utils.INPUT_BASE_KEY not in self._pipeline_node.parameters.parameters:
+      raise ValueError('input_base must be provided to example_gen')
+    input_base = (
+        self._pipeline_node.parameters.parameters[
+            utils.INPUT_BASE_KEY].field_value.string_value)
+    logging.debug('Processing input %s.', input_base)
+
+    # Note that this function updates the input_config.splits.pattern.
+    fingerprint, span, version = (
+        utils.calculate_splits_fingerprint_span_and_version(
+            input_base, input_config.splits))
+
+    # Populate exec_properties
+    result = driver_output_pb2.DriverOutput()
+    result.exec_properties[
+        utils.INPUT_CONFIG_KEY].string_value = json_format.MessageToJson(
+            input_config, sort_keys=True, preserving_proto_field_name=True)
+    result.exec_properties[utils.SPAN_PROPERTY_NAME].int_value = span
+    result.exec_properties[utils.VERSION_PROPERTY_NAME].int_value = version
+    result.exec_properties[
+        utils.FINGERPRINT_PROPERTY_NAME].string_value = fingerprint
+
+    # Populate output_dict
+    output_example = copy.deepcopy(
+        output_dict[utils.EXAMPLES_KEY][0].mlmd_artifact)
+    output_example.custom_properties[
+        utils.FINGERPRINT_PROPERTY_NAME].string_value = fingerprint
+    output_example.custom_properties[utils.SPAN_PROPERTY_NAME].int_value = span
+    if version:
+      output_example.custom_properties[
+          utils.VERSION_PROPERTY_NAME].int_value = version
+    result.output_artifacts[utils.EXAMPLES_KEY].artifacts.append(
+        output_example)
+    return result
